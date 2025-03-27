21747b73b7aad0704ac5705f1b473f8f767cb37e
Aram Hekimian <guerrerohek121@gmail.com>
2025-03-27 16:07:40 -0600

    Finished main chat opened design including messages and input for new message

--- frontend/src/components/ChatLabel.jsx
+++ frontend/src/components/ChatLabel.jsx
@@ -2,11 +2,13 @@ import styles from './css/ChatLabel.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faUserGroup } from '@fortawesome/free-solid-svg-icons';
 
-const ChatLabel = ({ chat }) => {
-  console.log(chat);
+const ChatLabel = ({ chat, active, handleClick, index }) => {
   const participant = chat.participants[0].user.username;
   return (
-    <li className={styles.chat_cont}>
+    <li
+      onClick={() => handleClick(index)}
+      className={`${styles.chat_cont} ${active && styles.active_chat}`}
+    >
       <div>
         <FontAwesomeIcon
           className={styles.icon}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,15 +7,22 @@ import {
   faArrowRight,
   faArrowLeft,
   faGear,
+  faUser,
+  faUserGroup,
+  faPaperPlane,
 } from '@fortawesome/free-solid-svg-icons';
 import astronaut from '../assets/astronaut.png';
 import { getChats } from '../api/chats';
 import ChatLabel from '../components/chatLabel';
+import Message from '../components/Message';
 
 const Home = () => {
   const { user, loading } = useAuth();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [activeChatIndex, setActiveChatIndex] = useState(null);
+  const [newMsg, setNewMsg] = useState('');
   const navigate = useNavigate();
   useEffect(() => {
     if (!user && !loading) {
@@ -50,6 +57,11 @@ const Home = () => {
     return <span className={styles.loader}></span>;
   }
 
+  const handleChatClick = (i) => {
+    setActiveChatIndex(i);
+    setSelectedChat(chats[i]);
+  };
+  console.log(selectedChat);
   return (
     <section className={styles.home_cont}>
       <div
@@ -65,9 +77,19 @@ const Home = () => {
           mobileMenuOpen && styles.mobile_menu_open
         }`}
       >
-        {chats.map((chat) => (
-          <ChatLabel key={chat.id} chat={chat} />
-        ))}
+        {chats.length > 0 ? (
+          chats.map((chat, index) => (
+            <ChatLabel
+              active={activeChatIndex === index}
+              key={chat.id}
+              chat={chat}
+              handleClick={handleChatClick}
+              index={index}
+            />
+          ))
+        ) : (
+          <span className={styles.loader}></span>
+        )}
       </ul>
       <FontAwesomeIcon
         onClick={() => {
@@ -77,9 +99,58 @@ const Home = () => {
         className={styles.settings_icon}
       />
       <div className={styles.chat_cont}>
-        <img src={astronaut} alt="hi" className={styles.astronaut} />
-        <p className={styles.empty_title}>No chat selected!</p>
-        <p className={styles.empty_subtitle}>Select one from the chats menu!</p>
+        {!selectedChat ? (
+          <>
+            <img src={astronaut} alt="hi" className={styles.astronaut} />
+            <p className={styles.empty_title}>No chat selected!</p>
+            <p className={styles.empty_subtitle}>
+              Select one from the chats menu!
+            </p>
+          </>
+        ) : (
+          <section className={styles.active_chat}>
+            <div className={styles.contact_info}>
+              <FontAwesomeIcon
+                className={styles.icon}
+                icon={selectedChat.isGroup ? faUserGroup : faUser}
+              />
+              <h2>
+                {selectedChat.isGroup
+                  ? selectedChat.name
+                  : selectedChat.participants[0].user.username}
+              </h2>
+            </div>
+            <ul className={styles.messages_cont}>
+              {selectedChat.messages.map((msg) => (
+                <Message
+                  key={msg.id}
+                  content={msg.content}
+                  senderId={msg.senderId}
+                  time={msg.createdAt}
+                  isOwn={msg.senderId === user.id}
+                />
+              ))}
+            </ul>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                setNewMsg('');
+              }}
+              className={styles.send_message_cont}
+            >
+              <input
+                type="text"
+                className={styles.send_message}
+                placeholder="Send message..."
+                value={newMsg}
+                onChange={(e) => setNewMsg(e.target.value)}
+              />
+              <button disabled={!newMsg}>
+                <FontAwesomeIcon icon={faPaperPlane} />
+              </button>
+            </form>
+          </section>
+        )}
       </div>
     </section>
   );
