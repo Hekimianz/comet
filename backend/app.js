55960d9fb0fd89fb529279f0b7dc42125b26eefb
Aram Hekimian <guerrerohek121@gmail.com>
2025-03-28 18:52:51 -0600

    changed from dev to prod url

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://comet-messenger.netlify.app/',
+    origin: 'https://comet-messenger.netlify.app',
     credentials: true,
   })
 );
